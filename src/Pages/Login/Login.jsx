92f7e5f6bbc9f40e87f1c965f57bfda6b4694b56
nguyenlong03 <nguyenlong682003@gmail.com>
2024-09-19 16:12:51 +0700

    data fix

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,13 +2,26 @@ import { useState } from "react";
 import "./Login.scss";
 import { FaGoogle, FaFacebookF, FaEyeSlash, FaEye } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
+import AutherAPi from "../../sevies/userlogin/userlogin";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState("false");
+  const [error, setError] = useState("");
 
-  const handleLogin = async () => {};
+  const handleLogin = async (e) => {
+    console.log("Login");
+    e.preventDefault();
+    try {
+      const response = await AutherAPi.login(email, password);
+      console.log("Login successful:", response.data);
+      localStorage.setItem("token", response.data.token);
+    } catch (error) {
+      setError("Login failed. Please check your credentials.");
+      console.error("Login error:", error);
+    }
+  };
 
   const handleShowPassword = () => {
     setShowPassword(showPassword === "false" ? "true" : "false");
@@ -21,7 +34,13 @@ function Login() {
         <p>Signup now and get full access to our app. </p>
         <div className="flex-col">
           <label>
-            <input type="email" placeholder="" required="" />
+            <input
+              type="email"
+              placeholder=""
+              required=""
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
             <span>Email</span>
           </label>
 
--- src/sevies/Products/Products.js
+++ src/sevies/Products/Products.js
@@ -2,23 +2,24 @@ import Axioscustom from "../axioscustom/Axioscustom";
 
 const ProductsApi = {
   getALL(params) {
-    const url = "posts";
+    const url = "/api/products?type=all&page=1";
+    console.log(`Request URL: ${Axioscustom.defaults.baseURL}${url}`); // In ra URL đầy đủ
     return Axioscustom.get(url, { params });
   },
   get(id) {
-    const url = `posts/${id}`;
+    const url = `"/api/products?type=all&page=1"/${id}`;
     return Axioscustom.get(url);
   },
   add(data) {
-    const url = "posts";
+    const url = "/api/products?type=all&page=1";
     return Axioscustom.post(url, data);
   },
   update(data) {
-    const url = `posts/${data.id}`;
+    const url = `/api/products?type=all&page=1/${data.id}`;
     return Axioscustom.patch(url, data);
   },
   remove(id) {
-    const url = `posts/${id}`;
+    const url = `/api/products?type=all&page=1/${id}`;
     return Axioscustom.delete(url);
   },
 };
