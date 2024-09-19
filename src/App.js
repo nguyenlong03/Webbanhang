9cea27a619b043309ba61ade3d9ef91d3acae0f4
nguyenlong03 <nguyenlong682003@gmail.com>
2024-09-19 17:28:45 +0700

    chuc nang login

--- src/App.js
+++ src/App.js
@@ -6,16 +6,6 @@ import { useEffect } from "react";
 import ProductsApi from "./sevies/Products/Products.js";
 import { useState } from "react";
 function App() {
-  const [data, setdata] = useState("");
-  useEffect(() => {
-    const getuser = async () => {
-      const produclist = await ProductsApi.getALL();
-      console.log("checck ress", produclist);
-      setdata(produclist.products);
-    };
-
-    getuser();
-  }, []);
 
   return (
     <div className="App">
@@ -27,20 +17,7 @@ function App() {
       </div>
 
       <hr className="border-primary custom-hr my-4" />
-      <div className="test-data">
-        {
-            data&&data.map((item , index)=>{
-            return  <div key={index}>
-            <ul>
-            
-              <li>{item.title}</li>
-              <img src={item.url_img} alt="" />
-            </ul>
-            </div>
-
-            })
-           }
-      </div>
+     
       <div className="app-footer">
         <Footer />
       </div>
