8d3cb7cfc6c8e3c6206351e95304122ac404df61
nguyenlong03 <nguyenlong682003@gmail.com>
2024-09-20 00:06:09 +0700

    codee

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,9 @@
 
 import './Home.scss';
 import React from "react";
-import SimpleSlider from "../../components/Slide/Slide";
-import HomeCart from "../../components/HomeCart/HomeCart";
-import Filter from '../../components/Filter/Filter';
+import SimpleSlider from "../../Components/Slide/Slide";
+import HomeCart from "../../Components/HomeCart/HomeCart";
+import Filter from '../../Components/Filter/Filter';
 
 const Home = () => {
   return <div className="Home-container">
